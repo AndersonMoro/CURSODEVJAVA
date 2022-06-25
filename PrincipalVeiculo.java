853215829961cb8ad2fc13005d4137019c6f5ef9
Anderson Moro <104262623+AndersonMoro@users.noreply.github.com>
2022-06-25 13:47:23 -0300

    Update PrincipalVeiculo.java

--- PrincipalVeiculo.java
+++ PrincipalVeiculo.java
@@ -1,20 +1,23 @@
 package abstracao;
 
 import java.util.Scanner;
-
-public class PrincipalVeiculo {
+public class VeiculoPrincipal {
 
 	public static void main(String[] args) {
 		Veiculo car = new Veiculo(1999,"Celta","Branco",80000);
 		Scanner ler = new Scanner(System.in);
+		Formatacao line = new Formatacao();
 		
-		car.exibirInfo();
+		
+		car.infoCar();
+		line.linhas();
+		line.linhas();
 		car.verificarManutencao();
-		System.out.print("Para qual cor quer mudar: ");
-		car.cor = ler.nextLine();
+		line.linhas();
+		line.linhas();
 		car.mudarCor();
 		car.exibirCor();
-		
 
 	}
+
 }
